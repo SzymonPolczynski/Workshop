afe070fdb5f45d294cb48e4d8bae4eb95dbc16d8
SzymonPolczynski <polczing@gmail.com>
2021-11-06 15:14:03 +0100

    final version

---
+++ dice.py
@@ -0,0 +1,37 @@
+import random
+
+DICES = ("D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100")
+
+
+def rolling_dice(dice_spec):
+    for dice in DICES:
+        if dice in dice_spec:
+            try:
+                multiply, modifier = dice_spec.split(dice)
+            except ValueError:
+                return "Wrong Input"
+            dice_value = int(dice[1:])
+            break
+    else:
+        return "Wrong Input"
+
+    try:
+        multiply = int(multiply) if multiply else 1
+    except ValueError:
+        return "Wrong Input"
+
+    try:
+        modifier = int(modifier) if modifier else 0
+    except ValueError:
+        return "Wrong Input"
+
+    return sum([random.randint(1, dice_value) for _ in range(multiply)]) + modifier
+
+
+if __name__ == '__main__':
+    print(rolling_dice("D6"))
+    print(rolling_dice("D16"))
+    print(rolling_dice("2D20+5"))
+    print(rolling_dice("2D12-2"))
+    print(rolling_dice("K10+5"))
+    print(rolling_dice("3D3+A"))
